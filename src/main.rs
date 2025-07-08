b61595585a4463c8dc247febc7a50ec3123fd00f
Samuel Åkesson <samak519@student.liu.se>
2025-07-08 22:04:00 +0200

    Going back n forth..

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+use config::Config;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[clap(version, author, about)]
+struct Args {
+    /// Config file
+    #[arg(short, long = "config", default_value = "picodocs.toml")]
+    config_path: PathBuf,
+
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Builds the site
+    Build {},
+
+    /// Dump the default configuration to a file
+    Defaults {
+        #[arg()]
+        output_path: PathBuf,
+    },
+}
+
+fn main() -> anyhow::Result<()> {
+    let _args = Args::parse();
+
+    let config = Config::builder()
+        .add_source(config::File::with_name("picodocs"))
+        .build()?;
+
+    dbg!(config);
+
+    Ok(())
 }
